a840e9232fdeaf1f43857a7651bf1e28aa84a112
hyoshiTyan <sakuramisa@mail.ru>
2021-02-19 21:40:43 +0300

    test

---
+++ Java3.1/src/ru/geekbrains/lesson1/Fruit.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.lesson1;
+
+public abstract class Fruit {
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+}
+
---
+++ Java3.1/src/ru/geekbrains/lesson1/Orange.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.lesson1;
+
+    public class Orange extends Fruit {
+
+        Orange() {
+            super(1.5f);
+        }
+
+        @Override
+        public String toString(){
+            return "Orange";
+        }
+
+    }
+
